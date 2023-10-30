e4ba0807c31eca012dda9f0386177d78df157d57
Babette Bækgaard <38117208+BabetteB@users.noreply.github.com>
2023-10-30 12:18:40 +0100

    Naddi og Thøvs done

--- Mini Project Note App/Mini Project Note App/App.xaml.cs
+++ Mini Project Note App/Mini Project Note App/App.xaml.cs
@@ -6,6 +6,18 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+		MainPage = new NavigationPage(new AppShell());
+    }
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        const int newWidth = 430;
+        const int newHeight = 932;
+
+        window.Width = newWidth;
+        window.Height = newHeight;
+
+        return window;
+    }
 }
---
+++ Mini Project Note App/Mini Project Note App/Notepage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Mini_Project_Note_App;
+
+public partial class Notepage : ContentPage
+{
+	public Notepage()
+	{
+		InitializeComponent();
+	}
+
+	public async void GoBack(object sender, EventArgs args)
+	{
+        await Navigation.PopModalAsync();
+    }
+
+}
+
